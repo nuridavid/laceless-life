58408f9a7270df6b4dabca03b83cc7553e9bc538
nuridavid <nurieldavid@gmail.com>
2021-04-27 21:48:28 -0400

    fixed bug with map not showing up on url params page

--- client/src/Components/Shoe/Shoe.jsx
+++ client/src/Components/Shoe/Shoe.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import "mapbox-gl/dist/mapbox-gl.css";
 import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import ShoePic from "../../Assets/toms shoes.jpeg";
 import ShoeMap from "./shoeMap/ShoeMap";
@@ -16,7 +17,7 @@ function Shoe({ history }) {
   const [viewport, setViewport] = useState({
     latitude: 26.146,
     longitude: -80.315,
-    zoom: 10,
+    zoom: 7,
     height: "40vh",
     width: "100%",
   });
@@ -32,7 +33,7 @@ function Shoe({ history }) {
       .catch(function (err) {
         console.log(err);
       });
-  }, [id]);
+  }, [id, shoe]);
   useEffect(() => {
     const listener = (e) => {
       if (e.key === "Escape") {
@@ -48,53 +49,52 @@ function Shoe({ history }) {
   return (
     shoe && (
       <div className="shoe__container">
-        <img src={shoe.img} alt></img>
+        <img src={shoe.img}></img>
         <h3>{shoe.shoe}</h3>
-        <div>
-          <ReactMapGL
-            {...viewport}
-            mapboxApiAccessToken={
-              "pk.eyJ1IjoibnVyaWRhdmlkIiwiYSI6ImNrbnhtOGM2aDB0MWUydm95anEzdXZlY2YifQ.3GQlEgjYKYrWYLKaag9Low"
-            }
-            mapStyle="mapbox://styles/nuridavid/ckny3gab03ori17pasqb4ydks"
-            onViewportChange={(viewport) => {
-              setViewport(viewport);
-            }}
+        {/* <div> */}
+        <ReactMapGL
+          {...viewport}
+          mapboxApiAccessToken={
+            "pk.eyJ1IjoibnVyaWRhdmlkIiwiYSI6ImNrbnhtOGM2aDB0MWUydm95anEzdXZlY2YifQ.3GQlEgjYKYrWYLKaag9Low"
+          }
+          mapStyle="mapbox://styles/nuridavid/ckny3gab03ori17pasqb4ydks"
+          onViewportChange={(viewport) => {
+            setViewport(viewport);
+          }}
+        >
+          <Marker
+            latitude={shoe.coordinates[1]}
+            longitude={shoe.coordinates[0]}
           >
-            <Marker
-              key={shoe.id}
-              latitude={shoe.coordinates[1]}
-              longitude={shoe.coordinates[0]}
+            <button
+              className="store__marker-btn"
+              onClick={(e) => {
+                e.preventDefault();
+                setSelectedShoe(Shoe);
+              }}
             >
-              <button
-                className="store__marker-btn"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setSelectedShoe(Shoe);
+              <img src={ShoePic} alt="shoe" />
+            </button>
+          </Marker>
+          <div>
+            {selectedShoe ? (
+              <Popup
+                latitude={shoe.coordinates[1]}
+                longitude={shoe.coordinates[0]}
+                key={shoe.id}
+                onClose={() => {
+                  setSelectedShoe(null);
                 }}
               >
-                <img src={Shoe} alt="shoe store icon" />
-              </button>
-            </Marker>
-            <div>
-              {selectedShoe ? (
-                <Popup
-                  latitude={shoe.coordinates[1]}
-                  longitude={shoe.coordinates[0]}
-                  key={shoe.id}
-                  onClose={() => {
-                    setSelectedShoe(null);
-                  }}
-                >
-                  <div>
-                    <h2>{shoe.shoe}</h2>
-                    <h4>{shoe.location}</h4>
-                  </div>
-                </Popup>
-              ) : null}
-            </div>
-          </ReactMapGL>
-        </div>
+                <div>
+                  <h2>{shoe.shoe}</h2>
+                  <h4>{shoe.location}</h4>
+                </div>
+              </Popup>
+            ) : null}
+          </div>
+        </ReactMapGL>
+        {/* </div> */}
       </div>
     )
   );
