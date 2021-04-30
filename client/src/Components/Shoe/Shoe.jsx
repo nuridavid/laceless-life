8a7bc08d77d68b5950f88756c115abca799280a6
nuridavid <nurieldavid@gmail.com>
2021-04-30 13:54:26 -0400

    fixed linking error

--- client/src/Components/Shoe/Shoe.jsx
+++ client/src/Components/Shoe/Shoe.jsx
@@ -8,7 +8,7 @@ import "./Shoe.scss";
 import { useParams } from "react-router-dom";
 import { Link } from "react";
 import { useState, useEffect } from "react";
-const url = "/api/shoes";
+const url = "http://localhost:8080/api/shoes";
 function Shoe({ props, match, history }) {
   const [viewport, setViewport] = useState({
     latitude: 26.146,
@@ -80,7 +80,7 @@ function Shoe({ props, match, history }) {
               <div className="info-box2">
                 <h3>Website</h3>
 
-                <h5> {shoe.link}</h5>
+                <h5> Take me to {shoe.shoe}'s Website</h5>
               </div>
             </a>
             <div className="info-box2">
