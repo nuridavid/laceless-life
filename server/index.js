b01675865f9afb92787b81741bd7b9cb6d9c64eb
nuridavid <nurieldavid@gmail.com>
2021-04-30 12:20:15 -0400

    will it work

--- server/index.js
+++ server/index.js
@@ -3,14 +3,25 @@ const app = express();
 const PORT = process.env.PORT;
 const shoesRoutes = require("./Routes/Shoes");
 const storesRoutes = require("./Routes/Stores");
+const path = require("path");
 app.use(express.json());
 const cors = require("cors");
 app.use(express.static("public"));
 app.use(cors());
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "..", "client", "build")));
+}
 app.use("/api/shoes", shoesRoutes);
 app.use("/api/stores", storesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  // Handle React routing, return all requests to React app
+  app.get("*", (request, response) => {
+    response.sendFile(
+      path.resolve(__dirname, "..", "client", "build", "index.html")
+    );
+  });
+}
 app.listen(PORT || 8080, () => {
   console.log(`Express is running on port ${PORT || 8080}`);
 });
