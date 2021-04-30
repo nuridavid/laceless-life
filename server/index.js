dfb7c676788e93bab7b6137c061d645b74d4b054
nuridavid <nurieldavid@gmail.com>
2021-04-30 00:36:56 -0400

    fixed port

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const shoesRoutes = require("./Routes/Shoes");
 const storesRoutes = require("./Routes/Stores");
 app.use(express.json());
@@ -11,8 +11,6 @@ app.use(cors());
 app.use("/api/shoes", shoesRoutes);
 app.use("/api/stores", storesRoutes);
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-app.listen(port);
+app.listen(PORT || 8080, () => {
+  console.log(`Express is running on port ${PORT || 8080}`);
+});
