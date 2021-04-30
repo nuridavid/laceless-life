3c03edef8adf47a317e6aebe4363817b25ce1aab
nuridavid <nurieldavid@gmail.com>
2021-04-29 21:29:09 -0400

    updated json

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 const shoesRoutes = require("./Routes/Shoes");
 const storesRoutes = require("./Routes/Stores");
 app.use(express.json());
