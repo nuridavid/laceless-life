4074ddededde1f168da4da2b6f51645a6c799a1a
nuridavid <nurieldavid@gmail.com>
2021-05-02 19:54:53 -0400

    final commit

--- client/src/Components/Shoe/Shoe.jsx
+++ client/src/Components/Shoe/Shoe.jsx
@@ -13,7 +13,7 @@ function Shoe({ props, match, history }) {
   const [viewport, setViewport] = useState({
     latitude: 26.146,
     longitude: -80.315,
-    zoom: 5,
+    zoom: 7,
     height: "400px",
     width: "100%",
   });
