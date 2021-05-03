e4a04912ecd056748bf6b7b7704d35f13a99dd53
nuridavid <nurieldavid@gmail.com>
2021-05-03 15:32:38 -0400

    fixed

--- client/src/Components/Stores/Stores.jsx
+++ client/src/Components/Stores/Stores.jsx
@@ -46,7 +46,7 @@ function Stores() {
       <ReactMapGL
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_KEY}
-        mapStyle="mapbox://styles/nuridavid/ckny3gab03ori17pasqb4ydks"
+        mapStyle={"mapbox://styles/nuridavid/ckny3gab03ori17pasqb4ydks"}
         onViewportChange={(viewport) => {
           setViewport(viewport);
         }}
